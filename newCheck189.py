441fb63f6d0986c4d9eb2aa3983b0126942e8df2
gilbinwin <gilbinwin@users.noreply.github.com>
2024-07-09 12:54:54 +0800

    Update newCheck189.py

--- newCheck189.py
+++ newCheck189.py
@@ -180,7 +180,8 @@ def main():
         description = response.json()['description']
         print(f"抽奖获得{description}")
         res2 = f"抽奖获得{description}"
-
+        
+    time.sleep(random.randint(5, 10))
     response = s.get(url2, headers=headers)
     if ("errorCode" in response.text):
         print(response.text)
@@ -190,6 +191,7 @@ def main():
         print(f"抽奖获得{description}")
         res3 = f"抽奖获得{description}"
 
+    time.sleep(random.randint(5, 10))
     response = s.get(url3, headers=headers)
     if ("errorCode" in response.text):
         print(response.text)
@@ -198,6 +200,31 @@ def main():
         description = response.json()['description']
         print(f"链接3抽奖获得{description}")
         res4 = f"链接3抽奖获得{description}"
+
+ if ddtoken.strip():
+        _ = ddtoken.strip()
+        # requests.get(f"https://push.xuthus.cc/send/{_}?c=func compute: {res1}, {res2}, {res3}")
+        # data = {"msgtype": "markdown", "markdown": { "title":f"sing189","text":f"hk1:sing189 \n> {res1} \n>{res2}{res3}{res4}"}}
+        # requests.post(f"https://oapi.dingtalk.com/robot/send?access_token={_}", json=data,)
+        timestamp = str(round(time.time() * 1000))
+        secret_enc = ddsecret.encode('utf-8')
+        string_to_sign = '{}\n{}'.format(timestamp, ddsecret)
+        string_to_sign_enc = string_to_sign.encode('utf-8')
+        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+        sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+        url = f'https://oapi.dingtalk.com/robot/send'
+        params={"access_token":ddtoken,"timestamp":timestamp,"sign":sign}
+        headers = {"Content-Type": "application/json;charset=utf-8"}
+        data = {"msgtype": "markdown",
+                "markdown": {"title": f"sing189", "text": f"sing189 \n> {res1} \n>{res2}{res3}{res4}"}}
+        response = requests.post(
+            url=url, data=json.dumps(data), headers=headers, timeout=15
+        ).json()
+ 
+        if not response["errcode"]:
+            print("钉钉机器人 推送成功！")
+        else:
+            print("钉钉机器人 推送失败！")
 
 
 
